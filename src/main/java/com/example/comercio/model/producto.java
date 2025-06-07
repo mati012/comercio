12425b4b4e13b30aa527a726ede90d23cb956203
mati012 <mati.quiero.arevalo@gmail.com>
2025-06-07 10:44:38 -0400

    cambios

--- src/main/java/com/example/comercio/model/producto.java
+++ src/main/java/com/example/comercio/model/producto.java
@@ -9,6 +9,9 @@ public class producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+   @Lob
+@Column(name = "imagen_url")
+private String imagenUrl;
 
     private String nombre;
     private String descripcion;
@@ -18,26 +21,71 @@ public class producto {
     @OneToMany(mappedBy = "producto")
     private List<carritoProducto> carritos;
     @ManyToOne
-        @JoinColumn(name = "catalogo_id")
-        private catalogo catalogo;
-    // Getters y Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-    public double getPrecio() { return precio; }
-    public void setPrecio(double precio) { this.precio = precio; }
-    public String getCategoria() { return categoria; }
-    public void setCategoria(String categoria) { this.categoria = categoria; }
-    public List<carritoProducto> getCarritos() { return carritos; }
-    public void setCarritos(List<carritoProducto> carritos) { this.carritos = carritos; }
-    public catalogo getCatalogo() {
-    return catalogo;
-}
+    @JoinColumn(name = "catalogo_id")
+    private catalogo catalogo;
 
-public void setCatalogo(catalogo catalogo) {
-    this.catalogo = catalogo;
-}
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<carritoProducto> getCarritos() {
+        return carritos;
+    }
+
+    public void setCarritos(List<carritoProducto> carritos) {
+        this.carritos = carritos;
+    }
+
+    public catalogo getCatalogo() {
+        return catalogo;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+    public void setCatalogo(catalogo catalogo) {
+        this.catalogo = catalogo;
+    }
 }
